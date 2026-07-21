2e0996d10982fc5468196b238a4d81fb4efb963e
Innocent <79470907+barzugini@users.noreply.github.com>
2026-07-21 18:42:04 +0700

    Настройка статического экспорта

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/atlas-izobreteniy" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
